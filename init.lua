e2a22f5b62629f16d7d05a95e2345d7a20bc328e
owl <97215481+coolestowl@users.noreply.github.com>
2023-03-30 00:41:00 +0800

    fix auto reload

--- init.lua
+++ init.lua
@@ -10,6 +10,20 @@ return {
         }
       }
     }
-  }
+  },
+  polish = function()
+    vim.o.autoread = true
+    vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+      command = "if mode() != 'c' | checktime | endif",
+      pattern = { "*" },
+    })
+    vim.api.nvim_create_autocmd({ "FileChangedShellPost" }, {
+      pattern = { "*" },
+      callback = function()
+        vim.notify("File changed on disk. Buffer reloaded.", vim.log.levels.INFO, {
+          title = "user/init.lua -> polish -> autocmd",
+        })
+      end,
+    })
+  end
 }
-
