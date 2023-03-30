b207e4d0e7f8de74a77ffe14e8269406680a5be1
owl <97215481+coolestowl@users.noreply.github.com>
2023-03-30 13:44:12 +0800

    add copilot

--- init.lua
+++ init.lua
@@ -2,28 +2,28 @@ return {
   plugins = {
     {
       "nvim-neo-tree/neo-tree.nvim",
-      opts = {
-        filesystem = {
-          filters = {
-            show_hidden = true,
-          }
-        }
-      }
-    }
+      opts = { filesystem = { filters = { show_hidden = true } } },
+    },
+    { "github/copilot.vim", lazy = false, config = function()
+    end },
   },
   polish = function()
     vim.o.autoread = true
-    vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
-      command = "if mode() != 'c' | checktime | endif",
-      pattern = { "*" },
-    })
+    vim.api.nvim_create_autocmd({
+      "BufEnter",
+      "CursorHold",
+      "CursorHoldI",
+      "FocusGained",
+    }, { command = "if mode() != 'c' | checktime | endif", pattern = { "*" } })
     vim.api.nvim_create_autocmd({ "FileChangedShellPost" }, {
       pattern = { "*" },
       callback = function()
-        vim.notify("File changed on disk. Buffer reloaded.", vim.log.levels.INFO, {
-          title = "user/init.lua -> polish -> autocmd",
-        })
+        vim.notify(
+          "File changed on disk. Buffer reloaded.",
+          vim.log.levels.INFO,
+          { title = "user/init.lua -> polish -> autocmd" }
+        )
       end,
     })
-  end
+  end,
 }
